8946dfac77d58c00af5bccf3c954d545f2ebf109
lash <dev@holbrook.no>
2022-03-01 18:45:33 +0000

    Add rpc cache test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -21,6 +21,7 @@ from chainlib.eth.tx import (
         )
 from chainlib.eth.block import (
         block_by_hash,
+        block_by_number,
         Block,
         )
 from chainlib.eth.address import is_same_address
@@ -28,6 +29,7 @@ from hexathon import strip_0x
 
 # local imports
 from eth_cache.store.file import FileStore
+from eth_cache.rpc import CacheRPC
 
 logging.basicConfig(level=logging.DEBUG)
 logg = logging.getLogger()
@@ -134,6 +136,22 @@ class TestCache(EthTesterCase):
         address = strip_0x(self.accounts[1])
         txs = self.store.get_address_tx(address)
         self.assertEqual(len(txs), 2)
+
+
+    def test_cache_rpc(self):
+        rpc = CacheRPC(None, self.store)
+
+        o = block_by_hash(self.block.hash)
+        block_src = self.rpc.do(o)
+        self.assertEqual(block_src['hash'], self.block.hash)
+
+        o = block_by_number(self.block.number)
+        block_src = self.rpc.do(o)
+        self.assertEqual(block_src['hash'], self.block.hash)
+
+        o = transaction(self.tx.hash)
+        tx_src = self.rpc.do(o)
+        self.assertTrue(is_same_address(tx_src['hash'], self.tx.hash))
 
 
 if __name__ == '__main__':
